efffcf4c1db60235a28e05740379d1f898ba7e7e
Ozgur Tanriverdi <ozgur.tanriverdi94@gmail.com>
2026-06-11 20:44:42 +0300

    entities array

--- main.c
+++ main.c
@@ -3,6 +3,12 @@
 #include <stdbool.h>
 #include <stdint.h>
 
+enum EntityKind {
+  ENEMY,
+  PLAYER,
+  BALL,
+};
+
 typedef struct {
   int32_t x;
   int32_t y;
@@ -12,6 +18,7 @@ typedef struct {
   int32_t vel_y;
   Color color;
   bool alive;
+  enum EntityKind kind;
 } Entity;
 
 #define SCREEN_WIDTH 900
@@ -27,12 +34,13 @@ typedef struct {
 #define ENEMY_PER_ROW 15
 #define ENEMY_ROW_COUNT 10
 #define ENEMY_COUNT (ENEMY_PER_ROW * ENEMY_ROW_COUNT)
+#define ENTITY_COUNT (ENEMY_COUNT + 2) // 2 for player and ball
 
 #define MIN(a, b) ((a) < (b) ? (a) : (b))
 #define MAX(a, b) ((a) > (b) ? (a) : (b))
 
-bool check_aabb(Entity a, Entity b) {
-  return (a.x < b.x + b.w && a.x + a.w > b.x && a.y < b.y + b.h && a.y + a.h > b.y);
+bool check_aabb(Entity *a, Entity *b) {
+  return (a->x < b->x + b->w && a->x + a->w > b->x && a->y < b->y + b->h && a->y + a->h > b->y);
 }
 
 int main(void) {
@@ -40,27 +48,33 @@ int main(void) {
 
   SetTargetFPS(60);
 
-  Entity player = (Entity){
+  // 2 for player and ball
+  Entity entities[ENTITY_COUNT];
+
+  entities[0] = (Entity){
       .w = PLAYER_WIDTH,
       .h = PLAYER_HEIGTH,
       .x = SCREEN_WIDTH / 2 - 60,
       .y = 1450,
       .color = GREEN,
       .alive = true,
+      .kind = PLAYER,
   };
 
-  Entity enemies[ENEMY_COUNT];
-
-  Entity ball = (Entity){
+  entities[1] = (Entity){
       .color = WHITE,
       .w = BALL_SIZE,
       .h = BALL_SIZE,
-      .x = player.x + (PLAYER_WIDTH / 2) - (BALL_SIZE / 2),
-      .y = player.y - BALL_SIZE,
+      .x = entities[0].x + (PLAYER_WIDTH / 2) - (BALL_SIZE / 2),
+      .y = entities[0].y - BALL_SIZE,
       .vel_x = -300.0f,
       .vel_y = -300.0f,
       .alive = true,
+      .kind = BALL,
   };
+
+  Entity *player = &entities[0];
+  Entity *ball = &entities[1];
 
   for (int i = 0; i < ENEMY_COUNT; i++) {
     int row = floor((float)i / ENEMY_PER_ROW) + 1;
@@ -69,62 +83,69 @@ int main(void) {
     int x = (col - 1) * (ENEMY_SIZE + ENEMY_PADDING) + (ENEMY_PADDING / 2);
     int y = (row - 1) * (ENEMY_SIZE + ENEMY_PADDING) + (ENEMY_PADDING / 2);
 
-    enemies[i] = (Entity){
+    entities[i + 2] = (Entity){
         .w = ENEMY_SIZE,
         .h = ENEMY_SIZE,
         .x = x,
         .y = y,
         .color = RED,
         .alive = true,
+        .kind = ENEMY,
     };
   }
 
   while (!WindowShouldClose()) {
     float dt = GetFrameTime();
 
-    if (IsKeyDown(KEY_D)) {
-      player.vel_x = PLAYER_VELOCITY;
-    } else if (IsKeyDown(KEY_A)) {
-      player.vel_x = -PLAYER_VELOCITY;
-    } else {
-      player.vel_x = 0;
-    }
-
-    if (ball.x <= 0 || ball.x >= SCREEN_WIDTH - BALL_SIZE) {
-      ball.vel_x = -ball.vel_x;
-    }
-
-    if (ball.y <= 0 || ball.y >= SCREEN_HEIGHT - BALL_SIZE) {
-      ball.vel_y = -ball.vel_y;
-    }
-
-    if (check_aabb(player, ball)) {
-      ball.vel_y = -ball.vel_y;
-    }
-
     BeginDrawing();
     ClearBackground(BLACK);
 
-    for (int i = 0; i < ENEMY_COUNT; i++) {
-      if (enemies[i].alive) {
-        if (check_aabb(ball, enemies[i])) {
-          ball.vel_y = -ball.vel_y;
-          enemies[i].alive = false;
+    for (int i = 0; i < ENTITY_COUNT; i++) {
+      Entity *entity = &entities[i];
+
+      switch (entity->kind) {
+      case PLAYER:
+        if (IsKeyDown(KEY_D)) {
+          player->vel_x = PLAYER_VELOCITY;
+        } else if (IsKeyDown(KEY_A)) {
+          player->vel_x = -PLAYER_VELOCITY;
+        } else {
+          player->vel_x = 0;
+        }
+        break;
+      case BALL:
+        if (entity->x <= 0 || entity->x >= SCREEN_WIDTH - BALL_SIZE) {
+          entity->vel_x = -entity->vel_x;
         }
 
-        DrawRectangle(enemies[i].x, enemies[i].y, enemies[i].w, enemies[i].h, enemies[i].color);
+        if (entity->y <= 0 || entity->y >= SCREEN_HEIGHT - BALL_SIZE) {
+          entity->vel_y = -entity->vel_y;
+        }
+
+        if (check_aabb(player, entity)) {
+          entity->vel_y = -entity->vel_y;
+        }
+        break;
+      case ENEMY:
+        if (entity->alive) {
+          if (check_aabb(ball, entity)) {
+            ball->vel_y = -ball->vel_y;
+            entity->alive = false;
+          }
+        }
+        break;
+      }
+
+      entity->x += entity->vel_x * dt;
+      entity->y += entity->vel_y * dt;
+      entity->x = MAX(0, entity->x);
+      entity->x = MIN(SCREEN_WIDTH - entity->w, entity->x);
+
+      if (entity->alive) {
+        DrawRectangle(entity->x, entity->y, entity->w, entity->h, entity->color);
       }
     }
 
-    player.x += player.vel_x * dt;
-    ball.x += ball.vel_x * dt;
-    ball.y += ball.vel_y * dt;
-
-    player.x = MAX(0, player.x);
-    player.x = MIN(SCREEN_WIDTH - PLAYER_WIDTH, player.x);
-
-    DrawRectangle(player.x, player.y, player.w, player.h, player.color);
-    DrawRectangle(ball.x, ball.y, ball.w, ball.h, ball.color);
     EndDrawing();
   }
 
